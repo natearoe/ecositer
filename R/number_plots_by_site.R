03f21e380df80b6315d2d47ab8a29b711d465c78
Nathan Roe <nateroe@gmail.com>
2023-01-20 11:55:21 -0800

    number_plots_by_site

---
+++ R/number_plots_by_site.R
@@ -0,0 +1,86 @@
+#' Number of plots by ecosite
+#'
+#' `number_plots_by_ecosite` describes the number of pedons and vegplots that
+#' have been recorded for each ecosite. It also lists the states/phases
+#'associated with each ecosite.
+#'
+#'
+#' @param static_location the location where a static NASIS database has been
+#' saved.
+#'
+#' @return a dataframe describing the number of pedons and vegplots recorded
+#' for each ecosite
+#' @export
+#'
+#' @examples
+#'
+#' number_plots_by_ecosite(static_location = "C:/Users/Nathan.Roe/Documents/SEKI/vegplotdata2.sqlite")
+#'
+#'
+number_plots_by_site <- function(static_location){
+
+  # Access veg data
+  veg_data <- veg_data <- fetchVegdata(dsn = static_location, SS = FALSE)
+
+  # Access fetchNASIS data
+  ecosite_data <- fetchNASIS(dsn = static_location,
+                             from = "pedons",
+                             SS = FALSE, fill = TRUE, duplicates = TRUE)
+
+
+  # Determine number of pedons associated with each ecosite
+  numb_pedons_by_ecosite <-
+    ecosite_id %>% count(ecositeid) %>% arrange(desc(n)) %>% dplyr::rename(pedons = n) %>%
+    mutate(ecosite_simple = stringr::str_sub(ecositeid, start = -6L, end = -3L))
+
+  # Determine the number of vegplots associated with each ecosite
+  numb_vegplots_by_ecosite <- veg_data_fecosite %>%
+    mutate(ecosite_simple = stringr::str_sub(
+      veg_data_fecosite$akfieldecositeid,
+      start = 1L,
+      end = 4L
+    )) %>%
+    count(ecosite_simple) %>% arrange(desc(n)) %>% dplyr::rename(vegplots = n)
+
+
+
+  # Remove a question mark from a couple of instances of akfieldecositeid
+  veg_data_fecosite$akfieldecositeid <-
+    veg_data_fecosite$akfieldecositeid %>%
+    stringr::str_replace(pattern = "\\.\\?", replacement = "")
+
+  # Identify the akfieldecositeids missing a phase
+  no_phase <-
+    veg_data_fecosite$akfieldecositeid[!veg_data_fecosite$akfieldecositeid %in%
+                                         stringr::str_subset(veg_data_fecosite$akfieldecositeid,
+                                                             "[:digit:]\\.[:digit:]\\.[:digit:]") &
+                                         !is.na(veg_data_fecosite$akfieldecositeid)]
+
+  # Add default phase to those missing - default being .1
+  veg_data_fecosite$akfieldecositeid_edit <-
+    ifelse(
+      veg_data_fecosite$akfieldecositeid %in%
+        no_phase,
+      paste0(veg_data_fecosite$akfieldecositeid, ".1"),
+      veg_data_fecosite$akfieldecositeid
+    )
+
+  # Summarise how many state/phases each ecosite has and list them
+  numb_statephases <-
+    veg_data_fecosite %>% dplyr::group_by(ecosite_simple) %>%
+    summarise(
+      numb_statephases = n_distinct(akfieldecositeid_edit),
+      statephases = paste(unique(akfieldecositeid_edit), collapse = ", ")
+    )
+
+
+  # Df showing number of pedons, vegplots, and states/phases for each ecosite
+  numb_pedons_and_vegplots <-
+    dplyr::full_join(numb_pedons_by_ecosite, numb_vegplots_by_ecosite) %>%
+    select(ecositeid, ecosite_simple, everything()) %>% dplyr::full_join(numb_statephases)
+
+
+
+  return(numb_pedons_and_vegplots)
+
+}
