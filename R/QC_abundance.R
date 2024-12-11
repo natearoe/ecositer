31017723bf43e8b0afcf1a3f853f4c39f5d8df6f
Roe <12001003012275@fedidcard.gov>
2024-12-11 14:55:09 -0800

    two new fxns

--- R/QC_abundance.R
+++ R/QC_abundance.R
@@ -4,8 +4,10 @@
 #'
 #' @return
 #' @export
+#' @seealso [QC_find_multiple_abundance()]
 #'
 #' @examples
+#'
 QC_aggregate_abundance <- function(veg_df){
 
   abund_cols <- c("akstratumcoverclasspct",
@@ -43,10 +45,45 @@ QC_aggregate_abundance <- function(veg_df){
            dplyr::select(siteiid, usiteid, siteobsiid, vegplotid, vegplotiid, # siteecositehistoryiid,
                          primarydatacollector, vegdataorigin, ecositeid, ecositenm, ecostateid, ecostatename, commphaseid, commphasename, plantsym,
                          plantsciname, plantnatvernm, pct_cover,
-                         horizdatnm, utmzone, utmeasting, utmnorthing)
+                         horizdatnm, utmzone, utmeasting, utmnorthing))
 
 }
 
 
-QC_find_multiple_abundance
+#' Find vegetation records with multiple abundances
+#'
+#' @param veg_df
+#'
+#' @return dataframe of records with multiple abundance columns populated
+#' @export
+#' @seealso [QC_aggregate_abundance()]
+#'
+#' @examples
+#'
+QC_find_multiple_abundance <- function(veg_df){
+
+  # abundance columns
+  abund_cols <- c("akstratumcoverclasspct",
+                  "speciescancovpct",
+                  "speciescomppct",
+                  "understorygrcovpct")
+
+  # Convert to data.table
+  data <- data.table::as.data.table(veg_df)
+
+  # Add a column counting non-NA values across abundance columns
+  data[, non_na_abundance_count := rowSums(!is.na(.SD)),
+       .SDcols = abund_cols]
+
+  # Return only the rows with multiple abundance columns populated
+  data[non_na_abundance_count > 1]
+
+  # Remove the helper column
+  data[, non_na_abundance_count := NULL]
+
+  return(data |> as.data.frame())
+
+}
+
+
 
