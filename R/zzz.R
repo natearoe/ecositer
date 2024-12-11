470761589ed8a258a594947125f4c27ad4e26b1c
Roe <12001003012275@fedidcard.gov>
2024-12-11 14:55:54 -0800

    remove

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-.onLoad <- function(libname, pkgname){
-  if(packageVersion("soilDB") <= "2.8.5") {
-    stop("ecositer requires at least version 2.8.6 of soilDB. ",
-         "Please reinstall ecositer to access the development version of soilDB - \n",
-         "remotes::install_github('natearoe/ecositer', dependencies = TRUE)"
-         )
-  }
+order_cols <- function(veg_df){
+
 }
