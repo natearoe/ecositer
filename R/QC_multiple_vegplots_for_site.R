41e3528b00310bd7060726a84ef270730a05b451
Roe <12001003012275@fedidcard.gov>
2024-12-11 09:11:07 -0800

    add fxn

--- R/QC_multiple_vegplots_for_site.R
+++ R/QC_multiple_vegplots_for_site.R
@@ -13,15 +13,15 @@ QC_multiple_vegplots_for_site <- function(veg_df, choose_best = FALSE) {
   # add checks to see if the veg_df is properly formatted.
 
   if(!choose_best){
-    data <- as.data.table(veg_df)
-    data[, data.table::uniqueN(vegplotiid) > 1, by = siteiid][, any(V1)]
+    data <- data.table::as.data.table(veg_df)
+    output <- data[, data.table::uniqueN(vegplotiid) > 1, by = siteiid][, any(V1)]
   }
 
   if(choose_best){
     # use QC_best_vegplot_for_site()
   }
 
-
+  return(output)
 }
 
 
