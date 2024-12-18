d6f5782cfae422358fb2b6f1db28e27dee0d388e
Roe <12001003012275@FEDIDCARD.GOV>
2024-12-17 21:38:47 -0800

    update completeness

--- R/QC_veg_completeness.R
+++ R/QC_veg_completeness.R
@@ -1,17 +1,12 @@
 #' @title Summarize vegetation plot completeness
-#'
-#'
-#'@description
+#' @description
 #' This function calculates summary statistics for vegetation plots grouped by `vegplotiid`, specifically:
 #' total_records - the total number of records associated with each `vegplotiid`
 #' unique_species - The number of unique species recorded for each `vegplotiid`.
 #' percent_to_species - the percentage of observations identified to the species level (determined by the presence of a space in `plantsciname`).
 #' percent_with_abund - the percentage of observations with a non-NA value in the `pct_cover` column.
 #'
-#'`QC_vegplots_remove_plots()` allows you to remove vegplots from your dataset
-#'based on siteiids and parameters related to vegplot completeness.
-#'
-#' @param veg_df
+#' @param veg_df vegetation dataframe
 #'
 #' @return a summary of vegetation plot completeness
 #' @export
@@ -29,7 +24,7 @@ QC_veg_completeness <- function(veg_df){
       unique_species = data.table::uniqueN(plantsym),  # Number of unique species
       percent_to_species = sum(grepl(" ", plantsciname)) / .N * 100,  # Percent identified to species
       percent_with_abund = sum(!is.na(pct_cover)) / .N * 100  # Percent with non-NA pct_cover
-    ), by = c("siteiid", "siteobsiid", "vegplotiid")]
+    ), by = c("siteiid", "siteobsiid", "vegplotiid", "primarydatacollector", "vegdataorigin")]
 
     return(result |> as.data.frame())
 
@@ -50,65 +45,64 @@ QC_veg_completeness <- function(veg_df){
 
 }
 
-
-#' @param veg_df veg_df created using `ecositer::create_veg_df()`
-#' @param my_QC_vegplots_summary results of `ecositer::my_QC_vegplots_summary()`
-#' @param min_species site must have >= N species to be included
-#' @param min_abund sites must have >= N% species with abundance data to be included (e.g.,
+#' @title Remove vegetation plots based on completeness criteria
+#' @param veg_df vegetation dataframe
+#' @param min_species (%) - sites must have >= min_species to be included
+#' @param min_abund integer - sites must have >= min_abund species with abundance data to be included (e.g.,
 #' entering 90 would include all sites with 90% or more species with abundance data).
 #' WARNING: If you keep sites that are missing abundance data and intend to run vegetation
-#' analyses using abundance data, abundance values will have to be populated. If you run
+#' analyses using abundance data, abundance values will have to be populated, otherwise that species record will be removed. If you run
 #' analyses on presence/absence data, abundance data is not needed.
-#' @param siteiids siteiids to be removed as defined by user
 #'
-#' @return `QC_vegplots_remove_plots()` returns a veg_df satisfying the defined
-#' requirements of completeness
+#' @return vegetation dataframe meeting completeness criteria
 #' @export
+#' @seealso [QC_veg_completeness()]
 #'
 #' @examples
-#' # example
-#' @rdname QC_vegplots_summary
+#' QC_completeness_criteria(veg_df = my_veg_df, min_unique_species = 5, min_perc_to_species = 70, min_perc_with_abund = 70)
 QC_completeness_criteria <- function(veg_df = NULL,
-                                     my_QC_vegplots_summary = NULL,
-                                     min_species = NULL,
-                                     min_perc_species_with_abund = NULL,
-                                     min_perc_species_level_id = NULL,
-                                     siteiids = NULL){
+                                     min_total_records = NULL,
+                                     min_unique_species = NULL,
+                                     min_perc_to_species = NULL,
+                                     min_perc_with_abund = NULL){
 
 
-  if(is.null(min_perc_species_level_id)){
-    min_perc_species_level_id = 0}
-  if(!round(min_perc_species_level_id) >= 0 | !min_perc_species_level_id%%1==0)
-    stop('min_perc_species_level_id must be a positive, whole number', call. = FALSE)
+  if(is.null(min_perc_to_species)){
+    min_perc_to_species = 0}
+  if(!round(min_perc_to_species) >= 0 | !min_perc_to_species%%1==0)
+    stop('min_perc_to_species must be a positive, whole number', call. = FALSE)
 
-  if(is.null(min_species)){
-    min_species = 0}
-  if(!round(min_species) >= 0 | !min_species%%1==0)
+  if(is.null(min_unique_species)){
+    min_unique_species = 0}
+  if(!round(min_unique_species) >= 0 | !min_unique_species%%1==0)
     stop('min_species must be a positive, whole number', call. = FALSE)
 
-  if(is.null(min_perc_species_with_abund)){
-    min_perc_species_with_abund = 0}
-  if(min_perc_species_with_abund < 0 | min_perc_species_with_abund > 100)
-    stop('min_perc_species_with_abund must range from 0-100.', call. = FALSE)
+  if(is.null(min_total_records)){
+    min_total_records = 0}
+  if(!round(min_total_records) >= 0 | !min_total_records%%1==0)
+    stop('min_species must be a positive, whole number', call. = FALSE)
+
+  if(is.null(min_perc_with_abund)){
+    min_perc_with_abund = 0}
+  if(min_perc_with_abund < 0 | min_perc_with_abund > 100)
+    stop('min_perc_with_abund must range from 0-100.', call. = FALSE)
 
 
   if(is.null(veg_df))
     stop('veg_df argument must be provided.', call. = FALSE)
 
-  if(is.null(my_QC_vegplots_summary))
-    stop('my_QC_vegplots_summary argument must be provided.', call. = FALSE)
 
-  if(is.null(siteiids)){
-    siteiids = c("")
-  }
+  veg_completeness_df <- ecositer::QC_veg_completeness(veg_df)
 
-  sites_with_min_requirement <- my_QC_vegplots_summary |>
-    dplyr::filter(species_richness >= min_species &
-                    perc_species_with_abund >= min_perc_species_with_abund &
-                    perc_species_level_id >= min_perc_species_level_id) |>
+
+  sites_with_min_criteria <- veg_completeness_df |>
+    dplyr::filter(total_records >= min_total_records &
+                    unique_species >= min_unique_species &
+                    percent_to_species >= min_perc_to_species &
+                    percent_with_abund >= min_perc_with_abund) |>
     dplyr::pull(siteiid)
 
-  veg_df |> dplyr::filter(siteiid %in% c(sites_with_min_requirement))
+  veg_df |> dplyr::filter(siteiid %in% sites_with_min_criteria)
 
 
 }
